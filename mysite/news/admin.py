3c613231eb7676143826f17fd2cf73a6e035fb5d
Sam Smith <mukhammad683@mail.ru>
2021-03-18 15:26:25 +0500

    newcom

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -10,6 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
+    list_editable = ('is_published',)
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
