128aa06846d06655902a3315b3539a28a9940e49
Sam Smith <mukhammad683@mail.ru>
2021-03-18 15:07:57 +0500

    added app

---
+++ mysite/news/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import News, Category
+# Register your models here.
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ mysite/news/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add  =True)
+    updated_at = models.DateTimeField(auto_now = True)
+    photo = models.ImageField(upload_to = 'photos/%y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default = True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+         verbose_name = "Новость"
+         verbose_name_plural = "Новости"
+         ordering = ['created_at']
+    
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Наименовании категории")
+    def __str__(self):
+        return self.title
+    class Meta:
+         verbose_name = "Категория"
+         verbose_name_plural = "Категории"
+         ordering = ['title']
+
+    
